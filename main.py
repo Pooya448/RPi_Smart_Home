f6d1b06bed78e66bde642ff74acac5258cab9776
Alireza1044 <alireza_1044@yahoo.com>
2021-01-02 21:47:19 +0330

    Add bot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import time
 from time import sleep, strftime
 from datetime import datetime
 
@@ -15,11 +16,42 @@ device.contrast(5)
 virtual = viewport(device, width=32, height=16)
 show_message(device, 'Raspberry Pi MAX7219', fill="white", font=proportional(LCD_FONT), scroll_delay=0.08)
 
-try:
-    while True:
-        with canvas(virtual) as draw:
-            text(draw, (0, 1), "Idris", fill="white", font=proportional(CP437_FONT))
-            #text(draw, (0, 1), datetime.now().strftime('%I:%M'), fill="white", font=proportional(CP437_FONT))
 
-except KeyboardInterrupt:
-    GPIO.cleanup()
+def handle(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
+
+    print('Got command: %s' % command)
+    text(draw, (0, 1), command, fill="white", font=proportional(CP437_FONT))
+
+    if command == 'on':
+        bot.sendMessage(chat_id, on(11))
+    elif command == 'off':
+        bot.sendMessage(chat_id, off(11))
+
+
+bot = telepot.Bot('1408435159:AAE6P_WYThTl8O2ldqheabyrg_iOUOtkZnk')
+bot.message_loop(handle)
+print('I am listening...')
+
+while 1:
+    try:
+        time.sleep(10)
+
+    except KeyboardInterrupt:
+        print('\n Program interrupted')
+        GPIO.cleanup()
+        exit()
+
+    except:
+        print('Other error or exception occured!')
+        GPIO.cleanup()
+
+# try:
+#     while True:
+#         with canvas(virtual) as draw:
+#             text(draw, (0, 1), "Idris", fill="white", font=proportional(CP437_FONT))
+#             #text(draw, (0, 1), datetime.now().strftime('%I:%M'), fill="white", font=proportional(CP437_FONT))
+#
+# except KeyboardInterrupt:
+#     GPIO.cleanup()
